0ebad357f3b2a5da3bc5fe753b2224ac05435404
maseology <mason@riffle.ca>
2019-06-10 13:42:01 -0400

    NSE fix

--- nse.go
+++ nse.go
@@ -8,6 +8,16 @@ func NSE(o, s []float64) float64 {
 	return NSEpow(o, s, 2.)
 }
 
+// NSEi "overloads" KGE
+func NSEi(oi, si []interface{}) float64 {
+	o, s := make([]float64, len(oi)), make([]float64, len(si))
+	for i := range oi {
+		o[i] = oi[i].(float64)
+		s[i] = si[i].(float64)
+	}
+	return NSEpow(o, s, 2.)
+}
+
 // NSEpow : the Nash-Sutcliffe efficiency measure
 // p: power
 // optimal maximization to 1.0
@@ -15,7 +25,7 @@ func NSEpow(o, s []float64, p float64) float64 {
 	var n, d float64
 	om, _ := meansd(o)
 	for i := range o {
-		if s[i] != -9999. && o[i] != -9999. {
+		if !math.IsNaN(s[i]) && !math.IsNaN(o[i]) {
 			n += math.Pow(s[i]-o[i], p)
 			d += math.Pow(o[i]-om, p)
 		}
