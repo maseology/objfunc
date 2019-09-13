9a95613caa4aba7a80bba4674278b0ea2f0eb0b5
maseology <mason@riffle.ca>
2019-09-13 18:16:46 -0400

    minor change

--- rmse.go
+++ rmse.go
@@ -4,12 +4,12 @@ import "math"
 
 // RMSE is the root mean squared errors
 func RMSE(o, s []float64) float64 {
-	c, n := 0, 0.
+	c, n := 0., 0.
 	for i := range o {
 		if !math.IsNaN(s[i]) && !math.IsNaN(o[i]) {
 			n += math.Pow(s[i]-o[i], 2.)
 			c++
 		}
 	}
-	return math.Sqrt(n / float64(c))
+	return math.Sqrt(n / c)
 }
